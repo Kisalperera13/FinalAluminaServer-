6d1e33af98d91b099a6af70616a177a451854a4c
KisalPerera <79179580+Kisalperera13@users.noreply.github.com>
2024-01-26 19:31:52 +0530

    AdminLoggingOk

--- controllers/auth.js
+++ controllers/auth.js
@@ -86,16 +86,16 @@ export const login = async (req, res) => {
     
         if (!admin) {
           return res.status(404).json({ message: "Admin not found" });
-        }
-    
-        const isPasswordValid = await admin.verifyPassword(password);
+        } 
+
+        const isPasswordValid = bcrypt.compare(password, admin.password);
     
         if (!isPasswordValid) {
           return res.status(401).json({ message: "Invalid credentials" });
         }
     
         const token = jwt.sign({ id: admin._id, username: admin.username }, process.env.JWT_SECRET);
-        res.status(200).json({ token });
+        res.status(200).json({ token, admin });
       } catch (error) {
         res.status(500).json({ error: error.message });
       }
